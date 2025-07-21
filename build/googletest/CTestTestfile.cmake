d9816b80eb9776ddb553cf3d2a79314b2049d13b
arn25-lk <arnavb25@gmail.com>
2025-07-21 13:58:47 +0200

    Remove build artifacts and generated files

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/arnav25/Desktop/3D_Renderer/googletest
-# Build directory: /Users/arnav25/Desktop/3D_Renderer/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/arnav25/Desktop/3D_Renderer/googletest/googletest
-# Build directory: /Users/arnav25/Desktop/3D_Renderer/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
